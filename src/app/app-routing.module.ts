5757cc82fd272f146fa2319a92b044718f3af112
aissam-ahbar <33417377+aissam-ahbar@users.noreply.github.com>
2022-12-05 00:14:41 +0100

    2 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'child',
+    loadChildren: './child/child-routing.module#ChildComponent',
+    //loadChildren: './views/book/book-routing.module#BookRoutingModule',
+  },
+  {
+    path: '**',
+    redirectTo: 'landing',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/child/child-routing.module.ts
+++ src/app/child/child-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChildComponent } from './child.component';
 
+// Create lazy loaded children component
 const routeChild: Routes = [
   {
     path: 'child',
     component: ChildComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'child',
   },
 ];
 
